8286d33170bf10ce66d64e2cf31b8017539f52ae
Emil Bjorneset <emilbjor@stud.ntnu.no>
2023-03-06 20:26:40 +0100

    updated
    fjerne gamle forsøk på scene

--- Include/PingPongScene.hpp
+++ Include/PingPongScene.hpp
@@ -11,57 +11,6 @@
 using namespace threepp;
 
 
-/*
-class PingPongScene : public Scene, public KeyListener {
-public:
-    explicit PingPongScene(PingPongGame &game) : game_(game){
 
-        int size =game.gridSize();
-        auto grid = GridHelper::create(size, size, 0x444444, 0x444444);
-        grid-> rotation.x =math::PI / 2;
-        grid->position.set(static_cast<float>(size) / 2, static_cast<float>(size) / 2, 0);
-        add(grid);
-
-        //Creating the object Paddle one
-        PaddleOneGeometry_ = BoxGeometry::create();
-        PaddleOneGeometry_->translate(0.5, 0.5, 0);
-
-        auto PaddleOneMaterial = MeshBasicMaterial::create();
-        PaddleOneMaterial->color = Color::white;
-
-        PaddleOne_.emplace_back(Mesh::create(PaddleOneGeometry_, PaddleOneMaterial));
-        add(PaddleOne_.back());
-
-        //Creating the object Paddle Two
-        PaddleTwoGeometry_ = BoxGeometry::create();
-        PaddleTwoGeometry_->translate(0.5, 0.5, 0);
-
-        auto PaddleTwoMaterial = MeshBasicMaterial::create();
-        PaddleTwoMaterial->color = Color::white;
-
-        PaddleTwo_.emplace_back(Mesh::create(PaddleTwoGeometry_, PaddleTwoMaterial));
-        add(PaddleTwo_.back());
-
-        //Setting up the Camera
-        camera_ = OrthographicCamera::create(-size / 2, size / 2, -size / 2, size / 2);
-        camera_->position.set(static_cast<float>(size) / 2, static_cast<float>(size) / 2, static_cast<float>(size));
-        add(camera_);
-    }
-
-private:
-    //Setting up different variables to use in the program
-    PingPongGame &game_;
-    std::shared_ptr<Mesh> ball_;
-    std::shared_ptr<OrthographicCamera> camera_;
-
-    std::shared_ptr<BoxGeometry> PaddleOneGeometry_;
-    std::shared_ptr<MeshBasicMaterial> PaddleOneMaterial_;
-    std::vector<std::shared_ptr<Mesh>> PaddleOne_;
-
-    std::shared_ptr<BoxGeometry> PaddleTwoGeometry_;
-    std::shared_ptr<MeshBasicMaterial> PaddleTwoMaterial_;
-    std::vector<std::shared_ptr<Mesh>> PaddleTwo_;
-};
- */
 
 #endif //THREEPP_VCPKG_TEST_PINGPONGSCENE_HPP
--- main.cpp
+++ main.cpp
@@ -11,11 +11,6 @@ int main() {
  Canvas canvas;
  GLRenderer renderer(canvas);
 
- /*
- auto gridSize = canvas.getSize();
- auto& gridSize_ = gridSize;
-  */
-
  renderer.setClearColor(Color::black);
 
  auto camera = PerspectiveCamera::create();
@@ -27,21 +22,21 @@ int main() {
 
  renderer.enableTextRendering();
 
- //Creating the Scoreboard
- auto& Scoreboard = renderer.textHandle("Score");
- Scoreboard.setPosition(280, canvas.getSize().height-480);
- Scoreboard.color = Color::green;
- Scoreboard.scale = 2;
+//Creating the Scoreboard
+    auto& Scoreboard = renderer.textHandle("Score");
+    Scoreboard.setPosition(280, canvas.getSize().height-480);
+    Scoreboard.color = Color::green;
+    Scoreboard.scale = 2;
 
- //Player one Score 100
- int P1Score = 0;
- auto P1Score_  = std::to_string(P1Score);
- auto& PlayerOne = renderer.textHandle(P1Score_);
- PlayerOne.setPosition(100, canvas.getSize().height-480);
- PlayerOne.color = Color::green;
- PlayerOne.scale = 2;
+//Player one Score 100
+    int P1Score = 0;
+    auto P1Score_  = std::to_string(P1Score);
+    auto& PlayerOne = renderer.textHandle(P1Score_);
+    PlayerOne.setPosition(100, canvas.getSize().height-480);
+    PlayerOne.color = Color::green;
+    PlayerOne.scale = 2;
 
- //Player Two Score 460
+//Player Two Score 460
     int P2Score = 0;
     auto P2Score_  = std::to_string(P2Score);
     auto& PlayerTwo = renderer.textHandle(P2Score_);
