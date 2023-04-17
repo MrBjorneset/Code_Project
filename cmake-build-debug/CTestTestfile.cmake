657e9dc94ad9fac1c0ceb4f829714c90bf5140d6
Emil Bjorneset <emilbjor@stud.ntnu.no>
2023-04-17 12:42:50 +0200

    Summary

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Kode/GitHub/C++/Code_Project
+# Build directory: C:/Kode/GitHub/C++/Code_Project/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
+subdirs("_deps/catch2-build")
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,4 @@
 //
 // Created by EmilB on 17/04/2023.
 //
+#include <catch2/catch.hpp>
