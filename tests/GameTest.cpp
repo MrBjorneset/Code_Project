1a17edfbe2c6a37a2a4f7130ea07d6a45fd864af
Emil Bjorneset <emilbjor@stud.ntnu.no>
2023-04-17 16:35:03 +0200

    Update GameTest.cpp

--- tests/GameTest.cpp
+++ tests/GameTest.cpp
@@ -15,7 +15,7 @@ TEST_CASE("GameTest") {
         game.listener.p1DirectionUp = true;
         game.singelPlayerMovement();
         auto paddleOne = game.PingPongScene_->getGroup()->getObjectByName("paddleOne");
-        CHECK_NOFAIL(paddleOne->position.y + 0.5f);
+        CHECK_NOFAIL(paddleOne->position.y + 1);
     }
     SECTION("MultiPlayerMovement"){
         Game game;
@@ -26,7 +26,12 @@ TEST_CASE("GameTest") {
 
         auto paddleOne = game.PingPongScene_->getGroup()->getObjectByName("paddleOne");
         auto paddleTwo = game.PingPongScene_->getGroup()->getObjectByName("paddleTwo");
-        CHECK_NOFAIL(paddleOne->position.y + 0.5f);
-        CHECK_NOFAIL(paddleTwo->position.y + 0.5f);
+        CHECK_NOFAIL(paddleOne->position.y + 1);
+        CHECK_NOFAIL(paddleTwo->position.y + 1);
+    }
+    SECTION("CheckScore"){
+        Game game;
+        REQUIRE(game.P1Score_ == 0);
+        REQUIRE(game.P2Score_ == 0);
     }
 }
