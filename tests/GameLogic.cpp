f9d8183b45d441166b20cdc9223fc93a36fb1e46
Emil Bjorneset <emilbjor@stud.ntnu.no>
2023-05-07 23:19:17 +0200

    updated

--- tests/GameLogic.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
-#include "PingPongGame.hpp"
-
-TEST_CASE("Check paddle one movement", "[Movement]") {
-    Game game;
-    game.singlePlayerMovement(true, false);
-    REQUIRE(game.p1PaddleSpeedY == 0.5f);
-}
