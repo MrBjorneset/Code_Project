3244139d99bc181a57ce29e7209bca0f68327efa
Emil Bjorneset <emilbjor@stud.ntnu.no>
2023-05-11 11:17:02 +0200

    Summary

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Kode/GitHub/C++/Code_Project
-# Build directory: C:/Kode/GitHub/C++/Code_Project/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
